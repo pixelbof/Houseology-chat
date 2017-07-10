4149ba63a6013d04994b72cc677d9caa4f89886c
Bradley Horner <BradleyHorner@paypoint.com>
2017-07-10 13:25:37 +0100

    adding new chat interfaces

--- index.js
+++ index.js
@@ -2,17 +2,42 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var userList = [];
+var currentUser = "";
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.emit('welcome', { id: socket.id });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg.message);
+  });
+
+  socket.on('newUser', function(user){
+    currentUser = user.username;
+
+    userList.push(user.username);
+    io.emit('userList update', userList);
+  });
+
+  socket.on('userList update', function() {
+    io.emit('userList update', userList);
+  })
+
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    var index = array.indexOf(currentUser);
+
+    if (index > -1) {
+        array.splice(index, 1);
+    }
   });
 });
 
+
+//heroku :35465
 http.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
