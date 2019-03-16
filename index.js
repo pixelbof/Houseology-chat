e2564d912f64efd96028cde97a07b86a36515703
Bradley Horner <bahorner87@gmail.com>
2019-03-16 23:18:39 +0000

    changed chat functions for new applicaiton

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-let app = require('express')();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
+let guestLogins = 0;
+let registeredLogins = 0;
+
+server = app.listen(3001, function(){
+    console.log('server is running on port 3001')
+});
+
+io = socket(server);
 
 io.on('connection', (socket) => {
     //io.emit('message', {type:'connected', text: "Chat has successfully connected!"});
     // Log whenever a user connects
-    socket.on('welcome', function(user){
-        io.emit('message', {type:'new-user', text: user + " has just locked in!"});
-    });
-
-    // Log whenever a client disconnects from our websocket server
-    socket.on('disconnect', function(user){
-        console.log("someone left");
+    socket.on('WELCOME', (user) => {
+        //(user.guest) ? guestLogins += 1: registeredLogins += 1;
+        io.emit('RECEIVE_MESSAGE', {msgType:'new-user', author: 'System', message: user.user + " has just locked in!", timeSent: user.timeSent });
+        //io.emit('USER_COUNT_UPDATE', {guest: guestLogins, registered: registeredLogins});
     });
 
     // When we receive a 'message' event from our client, print out
     // the contents of that message and then echo it back to our client
     // using `io.emit()`
-    socket.on('message', (message) => {
-        console.log("Message Received: " + message);
-        io.emit('message', {type:'new-message', text: message});    
+    socket.on('SEND_MESSAGE', (data) => {
+        io.emit('RECEIVE_MESSAGE', {msgType:'new-message', author: data.author, bale: data.bale, message: data.message, timeSent: data.timeSent});    
     });
-});
 
-// Initialize our websocket server on port 5000
-http.listen(process.env.PORT || 3001, () => {
-    console.log('started on port 3001');
+    socket.on('USER_DISCONNECTED', (user) => {
+        //(user.guest) ? guestLogins -= 1: registeredLogins -= 1;
+        //io.emit('USER_COUNT_UPDATE', {guest: guestLogins, registered: registeredLogins});
+    })
 });
